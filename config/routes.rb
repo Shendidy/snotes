190ac47cbe38be76cfbe9b1b7400a290dbb5db37
Sherif Shendidy <sherif@shendidy.com>
2019-02-18 15:12:06 +0000

    created categories controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/edit'
+  get 'categories/new'
+  get 'categories/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :notes
-  
+  resources :notes, :users, :categories
+
   root to:'home#index'
 end
